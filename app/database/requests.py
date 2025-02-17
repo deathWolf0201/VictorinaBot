82a9dd9699e730acf9800f350d15fef46464278e
deathWolf0201 <128875479+deathWolf0201@users.noreply.github.com>
2025-02-17 17:50:48 +1000

    refactoring to PEP8

--- app/database/requests.py
+++ app/database/requests.py
@@ -2,12 +2,16 @@ from app.database.models import async_session
 from app.database.models import User
 from sqlalchemy import select, delete
 
+
 async def set_user(tg_id, user_name):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
         if not user:
-            session.add(User(tg_id=tg_id, user_name=user_name, last_message_id=0, question_count=1, scores=0, is_passing=False, is_passed=False))
+            session.add(
+                User(tg_id=tg_id, user_name=user_name, last_message_id=0, question_count=1, scores=0, is_passing=False,
+                     is_passed=False))
             await session.commit()
+
 
 async def set_is_passing(tg_id):
     async with async_session() as session:
@@ -15,6 +19,7 @@ async def set_is_passing(tg_id):
         user.is_passing = True
         session.add(user)
         await session.commit()
+
 
 async def set_is_passed(tg_id):
     async with async_session() as session:
@@ -24,21 +29,25 @@ async def set_is_passed(tg_id):
         session.add(user)
         await session.commit()
 
+
 async def get_is_passing(tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
     return user.is_passing
+
 
 async def get_is_passed(tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
     return user.is_passed
 
+
 async def set_default(tg_id, user_name):
     async with async_session() as session:
         await session.execute(delete(User))
         await session.commit()
     await set_user(tg_id, user_name)
+
 
 async def set_scores(tg_id, score):
     async with async_session() as session:
@@ -47,10 +56,12 @@ async def set_scores(tg_id, score):
         session.add(user)
         await session.commit()
 
+
 async def get_scores(tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
     return user.scores
+
 
 async def get_question_count(tg_id):
     async with async_session() as session:
@@ -65,12 +76,14 @@ async def set_question_count(tg_id, counter):
         session.add(user)
         await session.commit()
 
+
 async def set_last_message_id(tg_id, message_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
         user.last_message_id = message_id
         session.add(user)
         await session.commit()
+
 
 async def get_last_message_id(tg_id):
     async with async_session() as session:
--- app/handlers.py
+++ app/handlers.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
-from aiogram.exceptions import TelegramBadRequest
-import app.quiz as quiz
 from aiogram import Router, F
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import CommandStart
 from aiogram.types import (Message, ReplyKeyboardMarkup, InlineKeyboardButton,
                            KeyboardButton, InlineKeyboardMarkup, CallbackQuery, ReplyKeyboardRemove)
-from bot_dp import bot
+
 import app.database.requests as rq
+import app.quiz as quiz
+from bot_dp import bot
+
 router = Router()
 
 data = []
 res = []
 max_scores = 0
 total_questions = 0
+
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     global data, max_scores, total_questions, res
@@ -39,7 +43,7 @@ async def cmd_start(message: Message):
     if res != data:
         res = data.copy()
         if message.from_user.last_name is not None:
-            await rq.set_default(message.from_user.id, message.from_user.first_name+" "+message.from_user.last_name)
+            await rq.set_default(message.from_user.id, message.from_user.first_name + " " + message.from_user.last_name)
         else:
             await rq.set_default(message.from_user.id, message.from_user.first_name)
     else:
@@ -49,25 +53,28 @@ async def cmd_start(message: Message):
             await rq.set_user(message.from_user.id, message.from_user.first_name)
 
     if await rq.get_is_passed(message.from_user.id):
-        await message.answer(f'Второй раз проходить тот же тест нельзя. Ваш результат {await rq.get_scores(message.from_user.id)}/{max_scores} баллов', reply_markup=ReplyKeyboardRemove())
+        await message.answer(
+            f'Второй раз проходить тот же тест нельзя. Ваш результат {await rq.get_scores(message.from_user.id)}/{max_scores} баллов',
+            reply_markup=ReplyKeyboardRemove())
         return
     if await rq.get_is_passing(message.from_user.id):
         message_id = await rq.get_last_message_id(message.from_user.id)
-        await remove_inline_keyboard(chat_id=message.from_user.id,message_id=message_id)
+        await remove_inline_keyboard(chat_id=message.from_user.id, message_id=message_id)
         await message.answer(
             f'Вы уже проходите тестирование. Вы остановились на {await rq.get_question_count(message.from_user.id)} вопросе из {total_questions}. Нажмите "Продолжить"',
             reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Продолжить')]], resize_keyboard=True,
                                              one_time_keyboard=True))
         return
     else:
-        await message.answer('Добро пожаловать на Кванторину! Ответьте правильно более чем на 12 вопросов и заберите памятный сувенир от Кванториума. Нажмите "Начать", чтобы приступить к тестированию. <b>Внимание</b>: у вас будет только одна попытка.', reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Начать')]], resize_keyboard=True, one_time_keyboard=True), parse_mode='HTML')
-
+        await message.answer(
+            'Добро пожаловать на Кванторину! Ответьте правильно более чем на 8 вопросов и заберите памятный сувенир от Кванториума. Нажмите "Начать", чтобы приступить к тестированию. <b>Внимание</b>: у вас будет только одна попытка.',
+            reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Начать')]], resize_keyboard=True,
+                                             one_time_keyboard=True), parse_mode='HTML')
 
 
 @router.message(F.text == 'Начать')
 async def begin(message: Message):
     global res, max_scores
-
 
     if await rq.get_is_passing(message.from_user.id):
         await my_func(message)
@@ -76,60 +83,77 @@ async def begin(message: Message):
     await rq.set_is_passing(message.from_user.id)
     await my_func(message)
 
-@router.callback_query(F.data == "True")
+
+@router.callback_query(F.data.startswith("True"))
 async def right(callback: CallbackQuery):
     await callback.answer('')
     await remove_inline_keyboard(callback.from_user.id, callback.message.message_id)
-    await callback.message.answer(f'Это правильный ответ.')
-    await rq.set_scores(callback.from_user.id, res[await rq.get_question_count(callback.from_user.id)-1]['cost'])
+    await callback.message.answer(f'Это правильный ответ. ')
+    await rq.set_scores(callback.from_user.id, res[await rq.get_question_count(callback.from_user.id) - 1]['cost'])
     if await rq.get_question_count(callback.from_user.id) == len(data):
         await ending(callback.from_user.id)
     else:
         await rq.set_question_count(callback.from_user.id, 1)
-        await callback.message.answer(f'Нажмите "Далее", чтобы перейти к следующему вопросу.', reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Далее')]], resize_keyboard=True, one_time_keyboard=True))
+        await callback.message.answer(f'Нажмите "Далее", чтобы перейти к следующему вопросу.',
+                                      reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Далее')]],
+                                                                       resize_keyboard=True, one_time_keyboard=True))
 
-@router.callback_query(F.data == "False")
+
+@router.callback_query(F.data)
 async def wrong(callback: CallbackQuery):
     await callback.answer('')
     await remove_inline_keyboard(callback.from_user.id, callback.message.message_id)
-    await callback.message.answer(f'Это неправильный ответ. Правильный ответ: {res[await rq.get_question_count(callback.from_user.id)-1]['correct']}.')
+    await callback.message.answer(
+        f'Это неправильный ответ. Правильный ответ: {res[await rq.get_question_count(callback.from_user.id) - 1]['correct']}.')
     if await rq.get_question_count(callback.from_user.id) == len(data):
         await ending(callback.from_user.id)
     else:
         await rq.set_question_count(callback.from_user.id, 1)
-        await callback.message.answer(f'Нажмите "Далее", чтобы перейти к следующему вопросу.', reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Далее')]], resize_keyboard=True, one_time_keyboard=True))
+        await callback.message.answer(f'Нажмите "Далее", чтобы перейти к следующему вопросу.',
+                                      reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Далее')]],
+                                                                       resize_keyboard=True, one_time_keyboard=True))
+
 
 @router.message(F.text == 'Продолжить')
 @router.message(F.text == 'Далее')
 async def my_func(message: Message):
     if await rq.get_last_message_id(message.from_user.id) > 0:
-        await remove_inline_keyboard(chat_id=message.from_user.id, message_id=await rq.get_last_message_id(message.from_user.id))
-    question = res[await rq.get_question_count(message.from_user.id)-1]['question']
-    correct = res[await rq.get_question_count(message.from_user.id)-1]['correct']
-    cost = res[await rq.get_question_count(message.from_user.id)-1]['cost']
+        await remove_inline_keyboard(chat_id=message.from_user.id,
+                                     message_id=await rq.get_last_message_id(message.from_user.id))
+    question = res[await rq.get_question_count(message.from_user.id) - 1]['question']
+    correct = res[await rq.get_question_count(message.from_user.id) - 1]['correct']
+    cost = res[await rq.get_question_count(message.from_user.id) - 1]['cost']
     inline_keyboard = []
     quest1 = quiz.Question(question, [], cost)
-    for element in res[await rq.get_question_count(message.from_user.id)-1]['answers']:
-        quest1.answers.append(quiz.Answer(text=element, correct=element==correct))
-        inline_keyboard.append([InlineKeyboardButton(text=quest1.answers[-1].text, callback_data=f"{quest1.answers[-1].correct}")])
-    msg = await message.answer(f"<i><b>{await rq.get_question_count(message.from_user.id)}/{len(data)}</b></i>\n"+quest1.text, reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard), parse_mode='HTML')
+    for i, element in enumerate(res[await rq.get_question_count(message.from_user.id) - 1]['answers']):
+        quest1.answers.append(quiz.Answer(text=element, correct=element == correct))
+        inline_keyboard.append(
+            [InlineKeyboardButton(text=quest1.answers[-1].text, callback_data=f"{quest1.answers[-1].correct}{i}")])
+    msg = await message.answer(
+        f"<i><b>{await rq.get_question_count(message.from_user.id)}/{len(data)}</b></i>\n" + quest1.text,
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard), parse_mode='HTML')
     await rq.set_last_message_id(message.from_user.id, msg.message_id)
+
 
 @router.message()
 async def wrong_message(message: Message):
     await message.answer('Простите, ваша команда не распознана')
 
+
 async def ending(tg_id):
     await rq.set_is_passed(tg_id)
-    if await rq.get_scores(tg_id)/max_scores*100 > 86:
+    if await rq.get_scores(tg_id) / max_scores * 100 > 86:
         await bot.send_message(
-            text=f'Викторина завершена. Ваш результат: {await rq.get_scores(tg_id)}/{max_scores} баллов. Поздравляем вас с результатом! Можете подойти на ресепшн за призом!',
+            text=f'Викторина завершена. Ваш результат: {await rq.get_scores(tg_id)}/{max_scores} баллов. Поздравляем вас с результатом!',
             reply_markup=ReplyKeyboardRemove(), chat_id=tg_id)
     else:
-        await bot.send_message(text=f'Викторина завершена. Ваш результат: {await rq.get_scores(tg_id)}/{max_scores} баллов. Спасибо, что поучаствовали в нашей викторине! К сожалению, вам не хватило баллов, чтобы получить приз.', reply_markup=ReplyKeyboardRemove(), chat_id=tg_id)
+        await bot.send_message(
+            text=f'Викторина завершена. Ваш результат: {await rq.get_scores(tg_id)}/{max_scores} баллов. Спасибо, что поучаствовали в нашей викторине! К сожалению, вам не хватило баллов до победы.',
+            reply_markup=ReplyKeyboardRemove(), chat_id=tg_id)
+
 
 async def remove_inline_keyboard(chat_id, message_id):
     try:
-        await bot.edit_message_reply_markup(chat_id=chat_id,message_id=message_id)
+        await bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id)
     except TelegramBadRequest:
         return
